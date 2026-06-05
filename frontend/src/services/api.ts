cbea12fad906fb04cfd8a0f94c91f4d6fdb924ec
Felipe Limachi <alesslim13@gmail.com>
2026-06-05 19:32:36 -0400

    corregir url del backend en frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "'https://backend-taw251.onrender.com/api'",
+    baseURL: "https://backend-taw251.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
